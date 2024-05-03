05a2964ce341eaf379d8e9c561191e8f36883f6e
Sag@0418 <sagarathani0418@gmail.com>
2024-05-03 22:34:07 +0530

    updated the frontend

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -8,5 +8,5 @@ export const personalData = {
   github: 'https://github.com/Sagarshivalingappaathani',
   linkedIn: 'https://www.linkedin.com/in/sagar-athani-4a5931251/',
   leetcode: "https://leetcode.com/sagar0418/",
-  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
+  resume: ""
 }
