4aa285dc6ea6b9405fb1e9e5755eca06f275ec68
Sag@0418 <sagarathani0418@gmail.com>
2024-05-07 18:35:58 +0530

    updated the expereiences

--- app/components/homepage/skills/index.jsx
+++ app/components/homepage/skills/index.jsx
@@ -1,3 +1,5 @@
+"use client";
+import { motion } from "framer-motion";
 import { skillsData } from "@/utils/data/skills";
 import { skillsImage } from "@/utils/skill-image";
 import Image from "next/image";
@@ -26,37 +28,42 @@ function Skills() {
           <span className="w-24 h-[2px] bg-[#1a1443]"></span>
         </div>
       </div>
-
-      <div className="w-full my-12">
-        <div className="w-full my-12 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-6">
-          {skillsData.map((skill, id) => (
-            <div
-              key={id}
-              className="flex flex-col items-center justify-center transition-all duration-500 rounded-lg group relative hover:scale-[1.15] cursor-pointer"
-            >
-              <div className="w-full rounded-lg border border-[#cc5cca] bg-[#11152c] shadow-none shadow-gray-500 group-hover:border-pink-500 transition-all duration-500">
-                <div className="flex justify-center">
-                  <div className="w-3/4">
-                    <div className="h-[1px] w-full bg-gradient-to-r from-transparent via-violet-500 to-transparent" />
+      <motion.div
+        className="transition-colors duration-300 transform"
+        whileInView={{ y: [-30, 0], opacity: [0, 1] }}
+        transition={{ duration: 0.75 }}
+      >
+        <div className="w-full my-12">
+          <div className="w-full my-12 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-6">
+            {skillsData.map((skill, id) => (
+              <div
+                key={id}
+                className="flex flex-col items-center justify-center transition-all duration-500 rounded-lg group relative hover:scale-[1.15] cursor-pointer"
+              >
+                <div className="w-full rounded-lg border border-[#cc5cca] bg-[#11152c] shadow-none shadow-gray-500 group-hover:border-pink-500 transition-all duration-500">
+                  <div className="flex justify-center">
+                    <div className="w-3/4">
+                      <div className="h-[1px] w-full bg-gradient-to-r from-transparent via-violet-500 to-transparent" />
+                    </div>
                   </div>
-                </div>
-                <div className="flex flex-col items-center justify-center gap-3 p-6">
-                  <div className="h-8 sm:h-10">
-                    <Image
-                      src={skillsImage(skill)?.src}
-                      alt={skill}
-                      width={40}
-                      height={40}
-                      className="h-full w-auto rounded-lg"
-                    />
+                  <div className="flex flex-col items-center justify-center gap-3 p-6">
+                    <div className="h-8 sm:h-10">
+                      <Image
+                        src={skillsImage(skill)?.src}
+                        alt={skill}
+                        width={40}
+                        height={40}
+                        className="h-full w-auto rounded-lg"
+                      />
+                    </div>
+                    <p className="text-white text-sm sm:text-lg">{skill}</p>
                   </div>
-                  <p className="text-white text-sm sm:text-lg">{skill}</p>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 }
