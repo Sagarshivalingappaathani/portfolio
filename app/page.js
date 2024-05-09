34e86cc2bda8221e3be5eba29d1bdd309dcced2f
Sag@0418 <sagarathani0418@gmail.com>
2024-05-09 15:05:10 +0530

    updated the expereiences

--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
-import Education from "./components/homepage/education";
 import HeroSection from "./components/homepage/hero-section";
+import AboutSection from "./components/homepage/about";
+import Education from "./components/homepage/education";
+import Experiences from "./components/homepage/experiences";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
-import Experiences from "./components/homepage/experiences";
-import AboutSection from "./components/homepage/about";
 import ContactSection from "./components/homepage/contact";
 
 export default async function Home() {
