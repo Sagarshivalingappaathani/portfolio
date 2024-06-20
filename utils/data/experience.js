ea17c67e6a5d309d6ee41b5c262dcaab5253ba0c
Sag@0418 <sagarathani0418@gmail.com>
2024-06-20 09:42:41 +0530

    updated the experience section

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -7,6 +7,7 @@ export const experiences = [
     Description: [
       "Developed a Club Event Management System in a team of 3 using Next.js, MongoDB, Express.js,and Mycalendar.js library",
       "Implemented RBAC (Role Based Access Control) for effective admin and member role management",
+      "Included features like Event Scheduling (for admin), registration, Feedback System, and Responsive Design for a comprehensive user experience."
     ],
     image: "/image/ie.png",
   },
@@ -14,7 +15,7 @@ export const experiences = [
     id: 2,
     title: "Web Developer Intern",
     company: "NullClass",
-    duration: "(May. 2024 – Present)",
+    duration: "(May. 2024 – June. 2024)",
     Description: [
       "Created password reset functionality via email or phone.",
       "Tracked user login info and implemented time-based mobile access rules.",
