eec6d79c558bbd918f9f7cc68a40bd4c026e04b3
sagar0418 <121311033+Sagarshivalingappaathani@users.noreply.github.com>
2025-06-21 13:25:57 +0530

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -8,5 +8,5 @@ export const personalData = {
   github: 'https://github.com/Sagarshivalingappaathani',
   linkedIn: 'https://www.linkedin.com/in/sagar-athani-4a5931251/',
   leetcode: "https://leetcode.com/sagar0418/",
-  resume:"https://drive.google.com/file/d/1XQzszsPHfq4wtTHxIdZII6oMDe4AdtT8/view?usp=sharing"
+  resume:"https://drive.google.com/file/d/1XpUSnR809AxhVN_y1iJ4Ee8GFO3T10e7/view?usp=sharing"
 }
